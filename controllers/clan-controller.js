79a3a083bd50c3b5e509ad560de95018adb9abd6
Robert Hardin <robertjameshardin@aol.com>
2022-08-25 15:46:43 -0500

    Add points to existing entires now working

--- controllers/clan-controller.js
+++ controllers/clan-controller.js
@@ -9,16 +9,17 @@ const {
 //   getClans
 // } = require('./mongoose-controller');
 
+// Controller for /api
 const clanController = {
-
+  // Wake up the server
   wakeServer(req, res) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    res.send("I'm awake!").end();
+    res.send("I'm awake!");
   },
-
+  // Get all clans
   getClans(req, res) {
     Clan.findAll({})
       .then(clans => { 
@@ -30,7 +31,7 @@ const clanController = {
       })
       .catch((err) => res.status(500).json(err));
       },
-
+  // Get a single clan
   getSingleClan(req, res) {
     Clan.findOne({
         where: {
@@ -38,6 +39,7 @@ const clanController = {
         },
       })
       .then(clans => { 
+        console.log(clans);
         res.header("Access-Control-Allow-Origin", '*');
         res.header("Access-Control-Allow-Credentials", true);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -46,7 +48,7 @@ const clanController = {
       })
       .catch((err) => res.status(400).json(err));
   },
-
+  // Create a clan
   createClan(req, res) {
     Clan.create(req.body)
     .then(clans => { 
@@ -54,28 +56,25 @@ const clanController = {
       res.header("Access-Control-Allow-Credentials", true);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-      res.staus(200).json(clans)
+      res.json(clans);
     })
       .catch((err) => res.status(400).json(err));
   },
-
+  // Add points to a clan by id
   addPoints(req, res) {
-    Clan.update({
-      points: points + req.body.points
-    }, {
-      where: {
-        id: req.params.clanId
-      },
-    })
-    .then(clans => { 
+    Clan.findOne({where: {id: req.params.clanId}})
+    .then(clan => {
+      clan.points += req.body.points;
+      clan.save();
       res.header("Access-Control-Allow-Origin", '*');
       res.header("Access-Control-Allow-Credentials", true);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-      res.staus(200).json(clans)
+      res.json(clan);
     })
+    .catch((err) => res.status(400).json(err));
   },
-
+  // Update a clan
   updateClan(req, res) {
     Clan.update(req.body, {
         where: {
@@ -87,11 +86,11 @@ const clanController = {
         res.header("Access-Control-Allow-Credentials", true);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-        res.staus(200).json(clans)
+        res.json(clans);
       })
       .catch((err) => res.status(400).json(err));
   },
-
+  // Delete a clan
   deleteClan(req, res) {
     Clan.destroy({
         where: {
@@ -103,7 +102,7 @@ const clanController = {
         res.header("Access-Control-Allow-Credentials", true);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-        res.staus(200).json(clans)
+        res.json(clans)
       })
       .catch((err) => res.status(400).json(err));
   }
--- routes/api/index.js
+++ routes/api/index.js
@@ -10,13 +10,14 @@ const {
   deleteClan
 } = require('../../controllers/clan-controller.js');
 
+// /api/wake
+router.route("/wake").get(wakeServer);
+
 // /api/clans
 router.route("/").get(getClans).post(createClan);
 
 // /api/clans/:clanId
 router.route("/:clanId").get(getSingleClan).post(addPoints).put(updateClan).delete(deleteClan);
 
-// /api/wake
-router.route("/wake").get(wakeServer);
 
 module.exports = router;
